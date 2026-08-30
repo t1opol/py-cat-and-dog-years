865e2eef2d03bb63fc2ec09cb4af7948a1d17d12
Oleksandr Chuhuievets <aleksandr10chu@gmail.com>
2026-08-30 13:15:00 +0300

    Add tests for cat and dog human age

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
-from app.main import get_human_age
+import pytest
 
-# write your code here
+from app import main
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 28, [2, 2]),
+        (28, 29, [3, 3]),
+    ],
+)
+def test_get_human_age(cat_age, dog_age, expected):
+    assert main.get_human_age(cat_age, dog_age) == expected
